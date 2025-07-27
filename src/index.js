9760c2996bd69043c7d6fa08abc2328c697d2c89
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-07-27 07:02:53 -0700

    Within slide function, if user click on right arrow while on last image then first image should appear and if user click on back arrow while on the first image then last image should appear

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@ const images = document.querySelectorAll("[data-img-url]");
 
 images.forEach((div) => {
     div.style.backgroundImage = `url(${div.getAttribute('data-img-url')})`;
-})
-  
+});
+const slider = document.querySelector(".slider");
+const indexButtons = document.querySelectorAll(".nav-container > button");
+const arrowButtons = document.querySelectorAll("[data-index-change]");
+let currIndex = 0;
+
+function slide(nextIndex){
+    if(nextIndex < 0) nextIndex = images.length - 1;
+    if(nextIndex > images.length - 1) nextIndex = 0;
+    
+}
