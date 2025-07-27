d0b4427f64cd69ec52cf8c5b1cabe392750f2b82
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-07-27 02:33:57 -0700

    Get the images thorugh data-img-url property

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // index.js
 import "./styles.css";
 
+const images = document.querySelectorAll("[data-img-url]");
 
+images.forEach((div) => {
+    div.computedStyleMap.backgroundImage = `url(${div.getAttribute('data-img-url')})`;
+})
   
