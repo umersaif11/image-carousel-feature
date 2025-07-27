dc29e0c17b39f200f97a0b9f057f7485f0fadcc5
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-07-27 07:14:41 -0700

    Attach event listener to the navigation buttons

--- src/index.js
+++ src/index.js
@@ -14,5 +14,19 @@ let currIndex = 0;
 function slide(nextIndex){
     if(nextIndex < 0) nextIndex = images.length - 1;
     if(nextIndex > images.length - 1) nextIndex = 0;
-    
+    indexButtons[currIndex].style.backgroundColor = "";
+    indexButtons[nextIndex].style.backgroundColor = "white";
+
+    //if user click on second button then nextIndex = 1 so
+    // transform = translateX(1/3 * 100)% = translateX 33.33% so
+    //slider will more 33.33% to left so we've second image
+    slider.style.transform = `translateX(-${(nextIndex / images.length) * 100}%)`;
+    currIndex = nextIndex;
 }
+
+indexButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        slide(index);
+    })
+})
+
